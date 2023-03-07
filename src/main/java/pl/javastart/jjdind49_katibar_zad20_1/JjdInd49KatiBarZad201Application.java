6703d2ef1cf081fbc553682ba56c881c54e693f0
KatiBar <katarzyna.kinga.baranska@gmail.com>
2023-03-07 21:42:27 +0100

    Initial commit

---
+++ src/main/java/pl/javastart/jjdind49_katibar_zad20_1/JjdInd49KatiBarZad201Application.java
@@ -0,0 +1,13 @@
+package pl.javastart.jjdind49_katibar_zad20_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjdInd49KatiBarZad201Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjdInd49KatiBarZad201Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/jjdind49_katibar_zad20_1/JjdInd49KatiBarZad201ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.jjdind49_katibar_zad20_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjdInd49KatiBarZad201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
